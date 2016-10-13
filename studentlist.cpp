fdb68faf3845f73f4e1f5e1dbeb189f2c02cefb5
Harish Palani <355131@beaverton.k12.or.us>
2016-10-13 14:29:03 -0700

    working on input features

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,48 +1,75 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <string.h>
 
 using namespace std;
 
 struct student {
-  string first;
-  string last;
+  char first[80];
+  char last[80];
   int id;
   float gpa;
 };
 
-void add_student(vector<student> &students, string first, string last, int id, float gpa);
-void remove_student(vector<student> &students, string first, string last, int id, float gpa);
+void add_student(vector<student> &students, char first[80], char last[80], int id, float gpa);
+void remove_student(vector<student> &students, int id);
 void print_all(vector<student> &students);
+void initialize(vector<student> &students, char input[80]);
 
 int main() {
   vector<student> students;
-  
-  add_student(students, "Jason", "Galbraith", 487329, 5.00);
-  add_student(students, "Harish", "Palani", 876543, 2.00);
-  print_all(students);
 
-  remove_student(students, "Harish", "Palani", 876543, 2.00);
-  print_all(students);
+  char input[80];
+  cout << "Enter a command [ADD, PRINT, or DELETE]: ";
+  cin >> input;
+
+  initialize(students, input);
 }
 
-void add_student(vector<student> &students, string first, string last, int id, float gpa) {
+void initialize(vector<student> &students, char input[80]) {
+
+   char first[80];
+   char last[80];
+   int id;
+   float gpa;
+
+   if(strcmp(input, "ADD") == 0) {
+     cout << "First name: ";
+     cin >> first;
+
+     cout << "Last name: ";
+     cin >> last;
+
+     cout << "ID: ";
+     cin >> id;
+
+     cout << "GPA: ";
+     cin >> gpa;
+
+     add_student(students, first, last, id, gpa);
+     
+     main();
+   } else if (strcmp(input, "PRINT") == 0) {
+     print_all(students);
+   }
+
+ }
+
+void add_student(vector<student> &students, char first[80], char last[80], int id, float gpa) {
   students.push_back(student());
   
   int i = students.size() - 1;
   
-  students[i].first = first;
-  students[i].last = last;
+  strcpy(students[i].first, first);
+  strcpy(students[i].last, last);
   students[i].id = id;
-  students[i].gpa = gpa;  
+  students[i].gpa = gpa;
 }
 
-void remove_student(vector<student> &students, string first, string last, int id, float gpa) {
+void remove_student(vector<student> &students, int id) {
   for(int i = 0; i < students.size(); i++) {
-    if((students[i].first.compare(first) == 0) &&
-       (students[i].last.compare(last) == 0) &&
-       (students[i].id == id) &&
-       (students[i].gpa == gpa)) {
+    if(students[i].id == id) {
       students.erase(students.begin() + i);
     }
   }
